a977692926db2bef2c6b70c16c3ffe90c3b31edc
greeeg <gregoiremielle@gmail.com>
2019-12-15 11:31:30 +0100

    fix: fix cleanLabel regex

--- src/map/utils.ts
+++ src/map/utils.ts
@@ -1,5 +1,5 @@
 export const cleanLabel = (label: string): string => {
-  const regex = /([a-zA-Z- ]+)(\([a-zA-Z- ]+\))?/;
+  const regex = /([a-zA-Z-' ]+)(\([a-zA-Z-' ]+\))?/;
   const [full, first, last] = regex.exec(label) || ['', '', ''];
 
   return first.trim();
